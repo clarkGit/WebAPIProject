078f98bdfb977d511ee7d389d052aab6ef67a9e7
clarkGit <clarkpham@gmail.com>
2014-12-05 15:09:03 +0700

    change

--- WebAPIProject/WebAPIProject/Models/Product.cs
+++ WebAPIProject/WebAPIProject/Models/Product.cs
@@ -10,6 +10,5 @@ namespace WebAPIProject.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }
-        public decimal Price { get; set; }
     }
 }
